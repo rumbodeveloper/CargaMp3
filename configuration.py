c2c9524868a515668785f0f51aabc841dc35dd87
rumbodeveloper <rumbo.developer@rumbo181.com>
2018-02-05 22:05:16 +0100

    actualizacion de configuracion

--- configuration.py
+++ configuration.py
@@ -6,12 +6,17 @@
 #Date: '24/7/16'
 
 
+#REAL
+#SOURCE_DIRECTORY = "/Volumes/Public/Shared Music/musica"
+#SONGS_NAMES_FILE= "/Users/Angel_Luis/songfiles.json"
+#TARGET_DIRECTORY="/Volumes/WALKMAN/MUSIC"
 
-SOURCE_DIRECTORY = "/Volumes/Public/Shared Music/musica"
-TARGET_DIRECTORY="/Volumes/WALKMAN/MUSIC"
+#ALTERNATE TARGET
 #TARGET_DIRECTORY="/Volumes/NO NAME/Musica"
 
-SONGS_NAMES_FILE= "/Users/Angel_Luis/songfiles.json"
+#TEST
+SOURCE_DIRECTORY = "test_data/musica"
+SONGS_NAMES_FILE= "test_data/songfiles.json"
 
 MIN_TO_LOAD = 0
 MAX_TO_LOAD = 15
