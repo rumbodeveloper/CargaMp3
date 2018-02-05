a122b2901cc9aae51bdd20bf4810c2efdbccaa56
rumbodeveloper <rumbo.developer@rumbo181.com>
2018-02-05 22:15:22 +0100

    test pasados - mejora terminada

--- configuration.py
+++ configuration.py
@@ -7,16 +7,16 @@
 
 
 #REAL
-#SOURCE_DIRECTORY = "/Volumes/Public/Shared Music/musica"
-#SONGS_NAMES_FILE= "/Users/Angel_Luis/songfiles.json"
-#TARGET_DIRECTORY="/Volumes/WALKMAN/MUSIC"
+SOURCE_DIRECTORY = "/Volumes/Public/Shared Music/musica"
+SONGS_NAMES_FILE= "/Users/Angel_Luis/songfiles.json"
+TARGET_DIRECTORY="/Volumes/WALKMAN/MUSIC"
 
 #ALTERNATE TARGET
 #TARGET_DIRECTORY="/Volumes/NO NAME/Musica"
 
 #TEST
-SOURCE_DIRECTORY = "test_data/musica"
-SONGS_NAMES_FILE= "test_data/songfiles.json"
+#SOURCE_DIRECTORY = "test_data/musica"
+#SONGS_NAMES_FILE= "test_data/songfiles.json"
 
 MIN_TO_LOAD = 0
 MAX_TO_LOAD = 15
